1f80c0fe9405873e73a0c2177a48805cb3719fcf
vroberts17 <vroberts17@my.whitworth.edu>
2015-09-20 14:32:47 -0700

    //Completed Homework

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -115,20 +115,25 @@ void Phone_Directory::save()
 	*/
 string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
 {
-	string search;
-	search = name;
-	int index = find(name);
-	if (index != -1)
-		delete &index;
-	else 
-		return name;
+
 	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
-	for (int i = index; i < size - 1; i++)
-		the_directory[i] = the_directory[i + 1];
+	//Completed by Victoria Roberts
+	int index = find(name);
 
-	return "";
+	if (index > -1) {
+		{
+			for (int i = index; i < size - 1; i++)
+			the_directory[i] = the_directory[i + 1];
+		}
+		size = size - 1; 
+		return name;
+	}
+	else {
+		return " "; 
 
-	//Trevor Case did this
+	}
+
+
 }
 
 // Private method implementation
--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -56,20 +56,28 @@ private:
 	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
 	{
 	public:
-		Directory_Entry() {} // Default no-argument constructor 
+
+		std :: string name; 
+		std :: string number; 
+
+		Directory_Entry() {
+			name = "Joe Smith"; 
+			number = "111-222-3333"; 
+		}  // Default no-argument constructor 
 		Directory_Entry(std::string the_name, std::string the_number) {
-			// constructor not implemented yet
+			name = the_name; 
+			number = the_number; 
 		}
 		std::string get_name() const {
-			return ""; // method not implemented yet
+			return name; 
 		}
 		std::string get_number() const {
-			return ""; // method not implemented yet
+			return number; // method not implemented yet
 		}
 		void set_number(const std::string& new_number) {
-			// method not implemented yet
+			number = new_number; 
 		}
-	
+	//Completed by Victoria Roberts
 	private:
 
 	};
